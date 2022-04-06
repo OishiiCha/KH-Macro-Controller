8beb0c132542baaf159179f4ffc86b3a78056e9a
OishiiCha <86476845+OishiiCha@users.noreply.github.com>
2022-04-06 16:40:04 +0100

    Update code.py

--- code.py
+++ code.py
@@ -11,14 +11,14 @@ white = (255,255,255)
 black = (0,0,0)
 
 KEYBOARD_MAP = {
-    (0,0): (Keycode.LEFT_CONTROL, Keycode.SHIFT, Keycode.1,),      # Start/Stop Video
-    (1,0): (Keycode.LEFT_CONTROL, Keycode.SHIFT, Keycode.2,),      # Switch Camera
-    (2,0): (Keycode.LEFT_CONTROL, Keycode.SHIFT, Keycode.3,),      # Mute/Unmute Self
-    (3,0): (Keycode.LEFT_CONTROL, Keycode.SHIFT, Keycode.4,),      # Mute/Unmute All
-    (0,1): (Keycode.LEFT_CONTROL, Keycode.SHIFT, Keycode.5,),      # Start/Stop Screenshare
-    (1,1): (Keycode.LEFT_CONTROL, Keycode.SHIFT, Keycode.6,),      # Show/Hide Floating Controls
-    (2,1): (Keycode.LEFT_CONTROL, Keycode.SHIFT, Keycode.7,),      # Chat Panel
-    (3,1): (Keycode.LEFT_CONTROL, Keycode.SHIFT, Keycode.8,),      # Participant Panel
+    (0,0): (Keycode.LEFT_CONTROL, Keycode.SHIFT, Keycode.ONE,),      # Start/Stop Video
+    (1,0): (Keycode.LEFT_CONTROL, Keycode.SHIFT, Keycode.TWO,),      # Switch Camera
+    (2,0): (Keycode.LEFT_CONTROL, Keycode.SHIFT, Keycode.FOUR,),      # Mute/Unmute Self
+    (3,0): (Keycode.LEFT_CONTROL, Keycode.SHIFT, Keycode.FIVE,),      # Mute/Unmute All
+    (0,1): (Keycode.LEFT_CONTROL, Keycode.SHIFT, Keycode.SEVEN,),      # Start/Stop Screenshare
+    (1,1): (Keycode.LEFT_CONTROL, Keycode.SHIFT, Keycode.EIGHT,),      # Show/Hide Floating Controls
+    (2,1): (Keycode.LEFT_CONTROL, Keycode.SHIFT, Keycode.SIX,),      # Chat Panel
+    (3,1): (Keycode.LEFT_CONTROL, Keycode.SHIFT, Keycode.THREE,),      # Participant Panel
     #(0,2): (Keycode.SHIFT, Keycode.E,),
     #(1,2): (Keycode.R,),
     #(2,2): (Keycode.E,),
